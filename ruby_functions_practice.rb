8cb55ece6c6c1bf180a7ebaeb326d317c325c3f2
user <user@users-MacBook-Pro.local>
2018-11-27 14:40:37 +0000

    Length cubed completed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -53,12 +53,27 @@
   #   end
   # end
 
-def number_to_short_month_name(month_number)
-  if month_number == 1
-      return "Jan"
-    elsif month_number == 4
-      return "Apr"
-    elsif month_number == 10
-      return "Oct"
-    end
+# def number_to_short_month_name(month_number)
+#   if month_number == 1
+#       return "Jan"
+#     elsif month_number == 4
+#       return "Apr"
+#     elsif month_number == 10
+#       return "Oct"
+#     end
+# end
+
+# #Given the length of a side of a cube calculate the volume
+def volume_of_cube(length)
+  return length ** 3
 end
+
+# #Given the radius of a sphere calculate the volume
+# def test_volume_of_sphere()
+#   #add test code here
+# end
+#
+# #Given a value in farenheit, convert this into celsius.
+# def test_fahrenheit_to_celsius()
+#   #add test code here
+# end
